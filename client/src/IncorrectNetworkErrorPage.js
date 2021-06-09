8738be6fc762a389c4f800be23f73c6d3f53fe95
Dan Buchholz <buchhlz2@gmail.com>
2021-06-08 19:14:29 -0700

    fix: 'Ganache' displayed while on Ropsten network

--- client/src/IncorrectNetworkErrorPage.js
+++ client/src/IncorrectNetworkErrorPage.js
@@ -3,9 +3,9 @@ import React from 'react'
 const IncorrectNetworkErrorPage = () => {
 	return (
 		<div className='container'>
-			{/* TODO network can be ganache, rinkeby, or mainnet */}
+			{/* TODO network can be ganache or ropsten; mainnet in future(?) */}
 			<h3 className='text-center' style={{ marginTop: '40vh' }}>
-				Please make sure your current network is Ganache.
+				Please make sure you are connected to a deployed network.
 			</h3>
 		</div>
 	)
