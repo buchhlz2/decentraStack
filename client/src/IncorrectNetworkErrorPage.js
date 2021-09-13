0104b5eaa4268a3ca2167abb544de53a8c8f811f
Dan Buchholz <buchhlz2@gmail.com>
2021-09-13 11:29:27 -0700

    bug: try to force ipfs hosting change
    When a wallet is initially connected, it will tell the user to change to Ganache, but it should be generic since Ropsten is also possible. This causes user confusion.

--- client/src/IncorrectNetworkErrorPage.js
+++ client/src/IncorrectNetworkErrorPage.js
@@ -3,9 +3,9 @@ import React from 'react'
 const IncorrectNetworkErrorPage = () => {
 	return (
 		<div className='container'>
-			{/* TODO network can be ganache or ropsten; mainnet in future(?) */}
+			{/* network can be ganache or ropsten */}
 			<h3 className='text-center' style={{ marginTop: '40vh' }}>
-				Please make sure you are connected to a deployed network.
+				Please make sure that you are connected to a deployed network.
 			</h3>
 		</div>
 	)
